466249ec9f0b1d1019f3006cc7fa1069041fee94
Julien <julienpetit1@gmail.com>
2023-01-06 11:23:32 +0100

    page_produit_2

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Models\Order;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
@@ -11,17 +12,21 @@ class Product extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'total_price',
-        'total_quantity',
-        
-    ];
-
-    public function orders(): BelongsToMany {
-
+    public function orders(): BelongsToMany
+    {
         return $this->belongsToMany(Order::class)
-        ->withPivot('total_quantity', 'total_price');
+            ->withPivot('price', 'quantity');
+    }
 
+    // public function price(): Attribute
+    // {
+    //     return Attribute::make(
+    //         get: fn ($value) => str_replace('.', ',', $value / 100) . '€'
+    //     );
+    // }
 
+    public function getFormattedPriceAttribute(): string
+    {
+        return str_replace('.', ',', $this->price / 100) . '€';
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,22 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import { createApp } from 'vue';
+import AddToCart from './components/AddToCart.vue';
+
+
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+const app = createApp();
+
+
+app
+    .component('AddToCart', AddToCart)
+    .component('NavbarCart', NavbarCart)
+    .component('ShoppingCart', ShoppingCart)
+    .component('Checkout', Checkout)
+
+app.mount('#app');
